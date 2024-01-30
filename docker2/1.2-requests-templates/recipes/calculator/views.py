95c23087c855490cc18ad97cf28b8ed5b571d559
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2024-01-30 11:24:36 +0300

    Docker2 added

---
+++ docker2/1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+def recipe_view(request, recipe_name):
+    servings = int(request.GET.get('servings', 1))
+
+    recipe = DATA.get(recipe_name)
+    if not recipe:
+        return render(request, 'calculator/error.html', {'error': 'Рецепт не найден'})
+
+    recipe_info = {}
+    for ingredient, amount in recipe.items():
+        recipe_info[ingredient] = amount * servings
+
+    context = {'recipe': recipe_info}
+    return render(request, 'calculator/recipe.html', context)
